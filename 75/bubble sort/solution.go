ae22c7437dd4ce41c31ccab16fdf7e67d963bac3
Sanzhar <skantay03@gmail.com>
2024-03-25 14:12:21 +0500

    ush

--- 75/bubble sort/solution.go
+++ 75/bubble sort/solution.go
@@ -2,7 +2,7 @@ package leetcode
 
 func sortColors(nums []int)  {
     for i := 0; i < len(nums)-1; i++ {
-		for i2 := i; i2 < len(nums)-1-i; i2++ {
+		for i2 := 0; i2 < len(nums)-i-1; i2++ {
 			if nums[i2] >  nums[i2+1] {
 				nums[i2], nums[i2+1] = nums[i2+1], nums[i2] 
 			}
