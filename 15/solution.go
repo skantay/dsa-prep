2e6a95b9f9522c0f40d530c5cabb3d4de79b08ec
Sanzhar <skantay03@gmail.com>
2024-03-28 23:03:15 +0500

    adding a new solution

--- 15/solution.go
+++ 15/solution.go
@@ -1,1 +1,3 @@
 package leetcode
+
+// three pointers
--- 977/#2/solution.go
+++ 977/#2/solution.go
@@ -1,5 +1,29 @@
 package leetcode
 
 func sortedSquares(nums []int) []int {
-	// two pointere
+	i, j := 0, len(nums)-1
+	result := make([]int, len(nums))
+	ind := len(nums) - 1
+
+	for i <= j {
+		if abs(nums[i]) > abs(nums[j]) {
+			result[ind] = nums[i] * nums[i]
+			i++
+		} else {
+			result[ind] = nums[j] * nums[j]
+			j--
+		}
+
+		ind--
+	}
+
+	return result
+}
+
+func abs(n int) int {
+	if n < 0 {
+		return n * -1
+	}
+
+	return n
 }
