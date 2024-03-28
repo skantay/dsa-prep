a1fda83e9568324c46115d98dffee9572ff2f5d0
Sanzhar <skantay03@gmail.com>
2024-03-28 20:48:14 +0500

    to do 2pointers

--- 977/#2/solution.go
+++ 977/#2/solution.go
@@ -1,22 +1,5 @@
 package leetcode
 
 func sortedSquares(nums []int) []int {
-	for i, n := range nums {
-		nums[i] = n * n
-	}
-
-	for i := 0; i < len(nums); i++ {
-
-		minInd := i
-
-		for j := i + 1; j < len(nums); j++ {
-			if nums[j] < nums[minInd] {
-				minInd = j
-			}
-		}
-
-		nums[i], nums[minInd] = nums[minInd], nums[i]
-	}
-
-	return nums
+	// two pointere
 }
