d8c79a199394f704eff5a3a0074967fa09387b25
Sanzhar <skantay03@gmail.com>
2024-03-28 23:43:04 +0500

    push

--- 15/solution.go
+++ 15/solution.go
@@ -1,3 +1,40 @@
 package leetcode
 
-// three pointers
+import "sort"
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+
+	result := make([][]int, 0)
+
+	prevVal := nums[0]
+
+	for i := 0; i < len(nums)-2; i++ {
+		if i != 0 && nums[i] == prevVal {
+			continue
+		}
+		j, k := i+1, len(nums)-1
+
+		for j < k {
+			sum := nums[i] + nums[j] + nums[k]
+
+			if sum > 0 {
+				k--
+			} else if sum < 0 {
+				j++
+			} else {
+				result = append(result, []int{nums[i], nums[j], nums[k]})
+
+				for j < len(nums)-1 && nums[j] == nums[j+1] {
+					j++
+				}
+
+				j++
+			}
+		}
+
+		prevVal = nums[i]
+	}
+
+	return result
+}
