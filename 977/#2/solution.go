5f81b77a3cbae5b1e8449e24226ac958e3236cc2
skantay <skantay03@gmail.com>
2024-03-28 15:46:16 +0600

    adding selection sort algo

--- 977/#2/solution.go
+++ 977/#2/solution.go
@@ -1,9 +1,22 @@
 package leetcode
 
 func sortedSquares(nums []int) []int {
-	/*
-		TO-DO
-	*/
+	for i, n := range nums {
+		nums[i] = n * n
+	}
 
-	return nil
+	for i := 0; i < len(nums); i++ {
+
+		minInd := i
+
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] < nums[minInd] {
+				minInd = j
+			}
+		}
+
+		nums[i], nums[minInd] = nums[minInd], nums[i]
+	}
+
+	return nums
 }
