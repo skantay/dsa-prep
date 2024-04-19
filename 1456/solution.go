9b5235d8dd67899753d5f56c39e6147377fbeb58
Sanzhar <skantay03@gmail.com>
2024-04-19 23:54:10 +0500

    done 1456

---
+++ 1456/solution.go
@@ -0,0 +1,30 @@
+package leetcode
+
+func maxVowels(s string, k int) int {
+    var count, res, l int
+
+    for i := 0; i < len(s); i++ {
+        if is(rune(s[i])) {
+            count++
+        }
+
+        if i >= k {
+
+            if is(rune(s[i])) && is(rune(s[l])) {
+                count--
+            } else if !is(rune(s[i])) && is(rune(s[l])) {
+                count--
+            }
+
+            l++
+        }
+
+        res = max(res, count)
+    }
+
+    return res
+}
+
+func is(s rune) bool {
+    return s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u'
+}
