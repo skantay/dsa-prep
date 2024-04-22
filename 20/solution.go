77509f954133cd3f5bd7c976dff817f285dcea18
Sanzhar <skantay03@gmail.com>
2024-04-22 23:54:21 +0500

    upd: 20

--- 20/solution.go
+++ 20/solution.go
@@ -4,31 +4,42 @@ func isValid(s string) bool {
     if len(s) == 1 {
         return false
     }
+
+    var top int = -1
+
     stack := make([]rune, 0)
 
     for i := 0; i < len(s);i++ {
         char := rune(s[i])
+
         switch char {
         case ')':
-            if len(stack) != 0 && stack[len(stack)-1] == '(' {
-                stack = stack[:len(stack)-1]
+            if len(stack) != 0 && stack[top] == '(' {
+                stack = stack[:top]
+                top--
             } else {
                 stack = append(stack, char)
+                top++
             }
         case '}':
-            if len(stack) != 0 && stack[len(stack)-1] == '{' {
-                stack = stack[:len(stack)-1]
+            if len(stack) != 0 && stack[top] == '{' {
+                stack = stack[:top]
+                top--
             } else {
                 stack = append(stack, char)
+                top++
             }
         case ']':
-            if len(stack) != 0 && stack[len(stack)-1] == '[' {
-                stack = stack[:len(stack)-1]
+            if len(stack) != 0 && stack[top] == '[' {
+                stack = stack[:top]
+                top--
             } else {
                 stack = append(stack, char)
+                top++
             }
         default:
             stack = append(stack, char)
+            top++
         }
     }
 
