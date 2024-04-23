c6dc9451dceda2905f1f516d908d34c69bde0f8c
Sanzhar <skantay03@gmail.com>
2024-04-23 13:52:48 +0500

    done 150

---
+++ 150/solution.go
@@ -0,0 +1,38 @@
+package leetcode
+
+func evalRPN(tokens []string) int {
+    stack := make([]int, 0)
+    var top int = -1
+
+    for _, char := range tokens {
+        switch char {
+        case "+":
+            num := stack[top]
+            stack = stack[:top]
+            top--
+            stack[top] = stack[top] + num
+        case "-":
+            num := stack[top]
+            stack = stack[:top]
+            top--
+            stack[top] = stack[top] - num
+        case "*":
+            num := stack[top]
+            stack = stack[:top]
+            top--
+            stack[top] = stack[top] * num
+        case "/":
+            num := stack[top]
+            stack = stack[:top]
+            top--
+            stack[top] = stack[top] / num
+        default:
+            num, _ := strconv.Atoi(char)
+            stack = append(stack, num) 
+            top++
+        }
+    }
+
+    return stack[top]
+}
+
