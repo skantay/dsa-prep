5549c9d3e3373123c7c2b4b54df76084a63f3a88
Sanzhar <skantay03@gmail.com>
2024-05-06 16:47:40 +0500

    done

--- 0203/solution.go
+++ 0203/solution.go
@@ -1,5 +1,33 @@
 package leetcode
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeElements1(head *ListNode, val int) *ListNode {
+	var h, prev *ListNode = nil, head
+
+	var foundHead bool
+
+	for head != nil {
+		if head.Val == val {
+			prev.Next = head.Next
+			head = head.Next
+
+			continue
+		} else if !foundHead {
+			h = head
+			foundHead = true
+		}
+
+		prev = head
+		head = head.Next
+	}
+
+	return h
+}
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -7,25 +35,16 @@ package leetcode
  *     Next *ListNode
  * }
  */
-func removeElements(head *ListNode, val int) *ListNode {
-    var h, prev *ListNode = nil, head
+func removeElements2(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return nil
+	}
 
-    var foundHead bool
+	head.Next = removeElements2(head.Next, val)
 
-    for head != nil {
-        if head.Val == val {
-            prev.Next = head.Next
-            head = head.Next
+	if head.Val == val {
+		return head.Next
+	}
 
-            continue
-        } else if !foundHead {
-            h = head
-            foundHead = true
-        }
-
-        prev = head
-        head = head.Next
-    }
-
-    return h
+	return head
 }
